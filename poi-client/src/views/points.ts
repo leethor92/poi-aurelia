d21558143a2dc6cfa5a4b099ce7d59ad5c0245fa
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-05-17 21:21:30 +0100

    created custom elements for points form

--- poi-client/src/views/points.ts
+++ poi-client/src/views/points.ts
@@ -1,16 +1,5 @@
 import {Point} from "../services/point-types";
 
 export class Points {
-  pointName: string;
-  pointDetails: string;
   points: Point[] = [];
-
-  addPoint() {
-    const point = {
-      pointName: this.pointName,
-      pointDetails: this.pointDetails,
-    };
-    this.points.push(point);
-    console.log(point);
-  }
 }
