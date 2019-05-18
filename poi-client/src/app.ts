7ec3b49eca98637225c2671e66ce93a06826e47d
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-05-18 11:41:30 +0100

    review & point seperated into seperate views with navigation enabled

--- poi-client/src/app.ts
+++ poi-client/src/app.ts
@@ -1,2 +1,26 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'review'],
+        name: 'Review',
+        moduleId: PLATFORM.moduleName('views/review'),
+        nav: true,
+        title: 'Review'
+      },
+      {
+        route: 'points',
+        name: 'points',
+        moduleId: PLATFORM.moduleName('views/points'),
+        nav: true,
+        title: 'Point'
+      }
+    ]);
+    this.router = router;
+  }
 }
--- poi-client/src/views/review.ts
+++ poi-client/src/views/review.ts
@@ -1,5 +1,5 @@
 export class Review {
-  reviews: any [] = [];
+  reviews: Review [] = [];
 }
 
 
