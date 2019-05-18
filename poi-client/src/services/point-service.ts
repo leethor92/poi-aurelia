5aba75d8e86d25256c292db502a717a1051a90b0
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-05-18 17:49:42 +0100

    data preloaded from points.json & refactored code into point-service

--- poi-client/src/services/point-service.ts
+++ poi-client/src/services/point-service.ts
@@ -1,8 +1,34 @@
 import {Point, Review} from "./point-types";
+import { HttpClient } from 'aurelia-http-client';
+import { inject } from 'aurelia-framework';
 
+@inject(HttpClient)
 export class PointService {
   points : Point[] = [];
   categoryTypes = ['North','South','East','West'];
   reviews: Review [] = [];
   ratingTypes = ['Bad','Ok','Good'];
+
+  constructor(private httpClient: HttpClient) {
+    httpClient.configure(http => {
+      http.withBaseUrl('http://localhost:8080');
+    });
+    this.getPoints();
+  }
+
+  async getPoints() {
+    const response = await this.httpClient.get('/api/points.json');
+    this.points = await response.content;
+    console.log (this.points);
+  }
+
+  async review(reviewName: string, reviewDetails: string, rating: string, point: Point) {
+    const review = {
+      reviewName: reviewName,
+      reviewDetails: reviewDetails,
+      rating: rating,
+      point: point
+    };
+    this.reviews.push(review);
+  }
 }
